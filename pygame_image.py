ef9135b5b5b23c61b3bcf8f47754cdd4e35c0d29
ODA <c0b23035fa@edu.teu.ac.jp>
2024-11-19 15:55:51 +0900

    練習6：背景画像ワープ

--- pygame_image.py
+++ pygame_image.py
@@ -16,12 +16,13 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        x = tmr % 800
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kou_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
+            
 
 
 if __name__ == "__main__":
