58e482f7f700776c32c593dfbdb5741d472e4f07
ODA <c0b23035fa@edu.teu.ac.jp>
2024-11-19 16:18:58 +0900

    練習7：背景画像

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,19 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_1 = pg.transform.flip(bg_img, True, False)
     kou_img = pg.image.load("fig/3.png")
     kou_img = pg.transform.flip(kou_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr % 800
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(kou_img, [300, 200])
+        x = -(tmr % 3200)
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img_1, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bg_img_1, [x+4800, 0])
+        screen.blit(kou_img, [300+100 , 200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
