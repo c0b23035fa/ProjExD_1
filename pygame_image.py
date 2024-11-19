3a224e73a85c164e45725bc64a3491578479ddaf
ODA <c0b23035fa@edu.teu.ac.jp>
2024-11-19 16:35:36 +0900

    練習8：移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,28 @@ def main():
     bg_img_1 = pg.transform.flip(bg_img, True, False)
     kou_img = pg.image.load("fig/3.png")
     kou_img = pg.transform.flip(kou_img, True, False)
+    kou_rct = kou_img.get_rect()
+    kou_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+
+        if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
+            kou_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
+            kou_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
+            kou_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
+            kou_rct.move_ip(+1, 0)
         x = -(tmr % 3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img_1, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img_1, [x+4800, 0])
-        screen.blit(kou_img, [300+100 , 200])
+        screen.blit(kou_img, kou_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
